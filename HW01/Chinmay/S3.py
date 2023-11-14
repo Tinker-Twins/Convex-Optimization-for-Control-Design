61e12cd52b542a97102fbc38e98d2f0002cc61f6
Tinker Twins <tinkertwinsofficial@gmail.com>
2023-11-14 12:57:36 -0500

    CHINMAY: Update Solution to Q3

--- HW01/Chinmay/S3.py
+++ HW01/Chinmay/S3.py
@@ -9,21 +9,22 @@ B1 = np.array([[1], [0]])
 A2 = np.array([[-3, 2], [4, 1]])
 B2 = np.array([[0], [1]])
 
-# Define the decision variable for the control gain K
-n = A1.shape[0]  # = A2.shape[0] Dimension of state vector
-m = B1.shape[1]  # = B2.shape[1] Dimension of control input
-K1 = cp.Variable((m, n))
-K2 = cp.Variable((m, n))
+# Define dimensions
+n = 2  # Dimension of state vector
+m = 1  # Dimension of control input
 
-# Define the LMI matrices
+# Define the decision variables
 P1 = cp.Variable((n, n), symmetric=True)
+Z1 = cp.Variable((m, n))
+
 P2 = cp.Variable((n, n), symmetric=True)
+Z2 = cp.Variable((m, n))
 
 # Define the LMI constraints for SYS1
-constraints1 = [A1 @ P1 + P1 @ A1.T + B1 @ K1 + K1.T @ B1.T << 0, P1 >> 0.001 * np.eye(n)]
+constraints1 = [A1@P1 + P1@A1.T + B1@Z1 + Z1.T@B1.T << 0, P1 >> 0.0001*np.eye(n)]
 
 # Define the LMI constraints for SYS2
-constraints2 = [A2 @ P2 + P2 @ A2.T + B2 @ K2 + K2.T @ B2.T << 0, P2 >> 0.001 * np.eye(n)]
+constraints2 = [A2@P2 + P2@A2.T + B2@Z2 + Z2.T@B2.T << 0, P2 >> 0.0001*np.eye(n)]
 
 # Create an optimization problem for SYS1
 problem1 = cp.Problem(cp.Minimize(0), constraints1)
@@ -37,19 +38,26 @@ problem1.solve()
 # Solve the LMI for SYS2
 problem2.solve()
 
-# Check the optimization results and obtain the stabilizing control gain K
+# Check the optimization results for SYS1
 if problem1.status == cp.OPTIMAL:
-    print('SYS1 is stabilizable.')
-    K1 = K1.value
+    print('SYS1 is stabilizable')
+    K1 = Z1.value @ np.linalg.inv(P1.value)
     print("K1 = ", K1)
+    Acl1 = A1 + B1@K1
+    print("Acl1 = A1 + B1@K1 =\n", Acl1)
+    print("Eigenvalues of Acl1:", np.linalg.eig(Acl1)[0])
 else:
-    print('SYS1 is not stabilizable.')
+    print('SYS1 is not stabilizable')
     K1 = None
 
+# Check the optimization results for SYS2
 if problem2.status == cp.OPTIMAL:
-    print('SYS2 is stabilizable.')
-    K2 = K2.value
+    print('\nSYS2 is stabilizable')
+    K2 = Z2.value @ np.linalg.inv(P2.value)
     print("K2 = ", K2)
+    Acl2 = A2 + B2@K2
+    print("Acl2 = A2 + B2@K2 =\n", Acl2)
+    print("Eigenvalues of Acl2:", np.linalg.eig(Acl2)[0])
 else:
-    print('SYS2 is not stabilizable.')
+    print('SYS2 is not stabilizable')
     K2 = None
