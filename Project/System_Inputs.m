4e21b928001d45e5be25e36374f947b674847f33
Tinker Twins <tinkertwinsofficial@gmail.com>
2024-01-07 23:07:50 -0500

    Update project

--- Project/System_Inputs.m
+++
@@ -1,8 +0,0 @@
-function p = System_Inputs(t)
-    % System_Inputs: delta_fl, delta_fr, delta_rl, delta_rr and Fw
-    p = [1.0; ... % Fw (disturbance input)
-         0.0; ... % delta_fl (control input)
-         0.0; ... % delta_fr (control input)
-         0.0; ... % delta_rl (control input)
-         0.0];    % delta_rr (control input)
-end
